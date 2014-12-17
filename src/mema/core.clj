e725b0f6f238fa968c0154eeb3cd89a652fbece3
Christophe Turle <cturle@yahoo.com>
2014-12-17 17:56:16 +0100

    equality slide 1 implementation

--- src/mema/core.clj
+++ src/mema/core.clj
@@ -1,6 +1,163 @@
-(ns mema.core)
+(ns mema.core
+  (:refer-clojure :exclude [get]) )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; let's create our own hierarchy.
+(def mem-hierarchy (atom (-> (make-hierarchy)
+                             (derive :Atom :Any)
+                             (derive :Node :Atom)
+                             (derive :Link :Atom)
+                             (derive :Property :Any)
+                             (derive :is-active :Property) )))
+
+(defn pp-obj-dispatcher [M PP OBJ]
+  [PP (get-in M [OBJ :isa] :Any)] )
+
+(defmulti get #'pp-obj-dispatcher :default [:Any :Any] :hierarchy mem-hierarchy)
+
+(defmethod get [:Any :Any] [M PP OBJ]
+  (get-in M [OBJ PP]) )
+
+(defn pp-type-dispatcher [M PP TYPE]
+  [PP TYPE] )
+
+(defmulti get-all #'pp-type-dispatcher :default [:Any :Any] :hierarchy mem-hierarchy)
+
+; not defined by default so it raised an error if used
+
+; Nodes are Active or (not Active). There is few Active Nodes and we want to retrieve all active nodes.
+; so we update our representation as below :
+;   a Node N is active <=> N is a member of ':active-nodes'
+(defmethod get [:is-active :Node] [M PP OBJ]
+  (contains? (clojure.core/get M :active-nodes) OBJ) )
+
+; we can now implement get-all [is-active Node]
+(defmethod get-all [:is-active :Node] [M PP OBJ]
+  (clojure.core/get M :active-nodes) )
+
+; We also need to desactivate all Nodes.
+(defn desactive-nodes
+  "(Node ?X) => (not (Active ?X))"
+  [M]
+  (assoc M :active-nodes #{}) )
+
+; let's do the same thing with active links :
+(defmethod get [:is-active :Link] [M PP OBJ]
+  (contains? (clojure.core/get M :active-links) OBJ) )
+
+; we can now implement get-all [is-active Node]
+(defmethod get-all [:is-active :Link] [M PP OBJ]
+  (clojure.core/get M :active-links) )
+
+
+;; (activeA ?M X) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L3 {isa Link, atoms [R =], active false}
+;  L4 {isa Link, atoms [X Y], active false}
+;  X  {isa Node}
+;  R  {isa Node}
+;  Y  {isa Node}
+;  =  {isa Node}
+;  :_me {:active-nodes [X]} }
+
+;; (activeA ?M 3) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active false}
+;  L4 {isa Link, atoms [X Y], active false}
+;  X  {isa Node, active true},
+;  3  {isa Node, active true},
+;  R  {isa Node, active false},
+;  Y  {isa Node, active false},
+;  =  {isa Node, active false} }
+
+
+;;; (activeA (activeA ?M R) =)
+
+;; (desactive-nodes) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active false}
+;  L4 {isa Link, atoms [X Y], active false}
+;  X  {isa Node, active false},
+;  3  {isa Node, active false},
+;  R  {isa Node, active false},
+;  Y  {isa Node, active false},
+;  =  {isa Node, active false} }
+
+;; (activeA ?M R) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active false}
+;  L4 {isa Link, atoms [X Y], active false}
+;  X  {isa Node, active false},
+;  3  {isa Node, active false},
+;  R  {isa Node, active true},
+;  Y  {isa Node, active false},
+;  =  {isa Node, active false} }
+
+
+;; (activeA ?M =) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active true}
+;  L4 {isa Link, atoms [X Y], active true}
+;  X  {isa Node, active false},
+;  3  {isa Node, active false},
+;  R  {isa Node, active true},
+;  Y  {isa Node, active false},
+;  =  {isa Node, active true} }
+
+
+;;; (activeA ?M Y)
+
+;; (desactive-nodes) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active true}
+;  L4 {isa Link, atoms [X Y], active true}
+;  L5 {isa Link, atoms [Y 3], active true} }
+;  X  {isa Node, active true},
+;  3  {isa Node, active true},
+;  R  {isa Node, active false},
+;  Y  {isa Node, active true},
+;  =  {isa Node, active false} }
+
+;; (activeA ?M Y) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active true}
+;  L4 {isa Link, atoms [X Y], active true}
+;  L5 {isa Link, atoms [Y 3], active true} }
+;  X  {isa Node, active true},
+;  3  {isa Node, active true},
+;  R  {isa Node, active false},
+;  Y  {isa Node, active true},
+;  =  {isa Node, active false} }
+
+;;; reponse
+
+;; (desactive-nodes) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active true}
+;  L4 {isa Link, atoms [X Y], active true}
+;  L5 {isa Link, atoms [Y 3], active true} }
+;  X  {isa Node, active false},
+;  3  {isa Node, active false},
+;  R  {isa Node, active false},
+;  Y  {isa Node, active false},
+;  =  {isa Node, active false} }
+
+;; (activeA ?M Y) ->
+; {L1 {isa Link, atoms [L3 L4], active true}
+;  L2 {isa Link, atoms [X 3], active true} }
+;  L3 {isa Link, atoms [R =], active true}
+;  L4 {isa Link, atoms [X Y], active true}
+;  L5 {isa Link, atoms [Y 3], active true} }
+;  X  {isa Node, active true},
+;  3  {isa Node, active true},
+;  R  {isa Node, active false},
+;  Y  {isa Node, active true},
+;  =  {isa Node, active false} }
+
+
--- test/mema/core_test.clj
+++ test/mema/core_test.clj
@@ -1,7 +1,104 @@
 (ns mema.core-test
   (:require [clojure.test :refer :all]
-            [mema.core :refer :all]))
+            [mema.core :as m]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def M-test
+  {:L1 {:isa :Link, :atoms [:N1 :N2]}
+   :L2 {:isa :Link, :atoms [:N2 :N3]}
+   :L3 {:isa :Link, :atoms [:L1 :L2]}
+   :N1  {:isa :Node}
+   :N2  {:isa :Node}
+   :N3  {:isa :Node}
+   :active-nodes #{:N1}
+   :active-links #{:L1} })
+
+
+; (pp-obj-dispatcher-test)
+(deftest pp-obj-dispatcher-test
+  (testing "PP = :isa"
+    (is (= [:isa :Link]
+           (m/pp-obj-dispatcher M-test :isa :L1) ))
+    (is (= [:isa :Node]
+           (m/pp-obj-dispatcher M-test :isa :N1) ))
+    )
+  (testing "PP = :active"
+    (is (= [:is-active :Link]
+           (m/pp-obj-dispatcher M-test :is-active :L1) ))
+    (is (= [:is-active :Node]
+           (m/pp-obj-dispatcher M-test :is-active :N1) ))
+    ))
+
+; (p-type-dispatcher-test)
+(deftest pp-type-dispatcher-test
+  (testing "PP = :is-active"
+    (is (= [:is-active :Link]
+           (m/pp-type-dispatcher M-test :is-active :Link) ))
+    (is (= [:is-active :Node]
+           (m/pp-type-dispatcher M-test :is-active :Node) ))
+    ))
+
+; (get-test)
+(deftest get-test
+  (testing "PP = :isa"
+    (is (= :Link
+           (m/get M-test :isa :L1) ))
+    (is (= :Node
+           (m/get M-test :isa :N1) ))
+    )
+  (testing "PP = :is-active"
+    (is (= true
+           (m/get M-test :is-active :L1) ))
+    (is (= false
+           (m/get M-test :is-active :L2) ))
+    (is (= true
+           (m/get M-test :is-active :N1) ))
+    (is (= false
+           (m/get M-test :is-active :N2) ))
+    ))
+
+; (get-all-test)
+(deftest get-all-test
+  (testing "PP = :is-active"
+    (is (= #{:N1}
+           (m/get-all M-test :is-active :Node) ))
+    (is (= #{:L1}
+           (m/get-all M-test :is-active :Link) ))
+    ))
+
+
+
+;;; (equality-slides) cf doc/equality-states.pptx
+
+(deftest equality-slides
+  (let [M (atom {})]
+    (testing "SLIDE 1 : init"
+      (reset! M  {:L1 {:isa :Link, :atoms [:L3 :L4]}
+                  :L3 {:isa :Link, :atoms [:R :N1]}
+                  :L4 {:isa :Link, :atoms [:X :Y]}
+                  :X  {:isa :Node}
+                  :R  {:isa :Node}
+                  :Y  {:isa :Node}
+                  :N1 {:isa :Node, :io "="}
+                  :io=>node {"=" :N1}
+                  :active-nodes #{}
+                  :active-links #{:L1} })
+      (is (= #{}
+             (m/get-all @M :is-active :Node) ))
+      (is (= #{:L1}
+             (m/get-all @M :is-active :Link) ))
+      )))
+
+
+;;; (activeA (activeA ?M X) 3)
+;; (def M1 (desactive-nodes M0))
+
+; (run-tests)
+
+
+
+
+
+
+
+
