d9fdd3926d3b6d8ca4894f4f89be0ff871901ebb
Christophe Turle <cturle@yahoo.com>
2014-12-24 11:54:47 +0100

    Activation in progress ...

--- src/mema/core.clj
+++ src/mema/core.clj
@@ -1,5 +1,6 @@
 (ns mema.core
-  (:refer-clojure :exclude [get]) )
+  (:refer-clojure :exclude [get])
+  (:require [mema.util :refer :all]) )
 
 ;; let's create our own hierarchy.
 (def mem-hierarchy (atom (-> (make-hierarchy)
@@ -35,7 +36,7 @@
   (clojure.core/get M :active-nodes) )
 
 ; We also need to desactivate all Nodes.
-(defn desactive-nodes
+(defn desactivate-nodes
   "(Node ?X) => (not (Active ?X))"
   [M]
   (assoc M :active-nodes #{}) )
@@ -44,10 +45,83 @@
 (defmethod get [:is-active :Link] [M PP OBJ]
   (contains? (clojure.core/get M :active-links) OBJ) )
 
-; we can now implement get-all [is-active Node]
 (defmethod get-all [:is-active :Link] [M PP OBJ]
   (clojure.core/get M :active-links) )
 
+(defn get-lex=>node
+  "return Node designated by Lexical L if it exists, else returns nil."
+  [M L]
+  (get-in M [:lex=>node L]) )
+
+(defn get-link
+  "returns Link which link atoms A1 and A2 if it exists, else returns nil."
+  [M A1 A2]
+  (get-in M [:atoms=>link #{A1 A2}]) )
+
+(defn create-lex=>node
+  "create, in Memory M, a new Node indexed by Lexical L. L must be a String."
+  [M L]
+  (assert (string? L) L)
+  (let [N (gensym "Node")
+        M (assoc M N {:isa :Node, :lex L})
+        M (assoc-in M [:lex=>node L] N) ]
+    M ))
+
+
+;;; ACTIVATION
+
+(defn lexical? [X]
+  (string? X) )
+
+(defn concept? [X]
+  (or (symbol? X) (keyword? X)) )
+
+(defn activate-node
+  "Activates Node N in Memory M. N denotes a Lexical or a Concept"
+  [{:keys [M N GO], :as S, :or {GO :init}}]
+  (case GO
+    :init
+    (cond (lexical? N)
+            (recur (assoc S :GO :lexical))
+          (concept? N)
+            (recur (assoc S :GO :concept))
+          true
+            (throw (ex-info "unknow index type" S)) )
+    :lexical
+    (if-let [NODE (get-lex=>node M N)]
+      (recur (assoc S :GO :node, :N NODE))
+      (recur (assoc S :GO :lexical, :M (create-lex=>node M N))) )
+    :concept
+    (if (= :Node (get-in M [N :isa]))
+      (recur (assoc S :GO :node))
+      (throw (ex-info "concept is not a Node" S)) )
+    :node
+    (fassoc-in M [:active-nodes] conj N) ))
+
+
+;;; PROPAGATION
+
+(defn propagate-1
+  "propagate activation one step in a memory. More propagation may be needed, cf 'propagate' for full propagation."
+  [M]
+  M
+  )
+
+(defn propagate
+  "propagate activations in a memory until no more can be done."
+  [M]
+  M
+  )
+
+(defn process-place
+  "process argument as one of a ':X :R :Y' form. For example, (process-place :X '3') will process '3' as the place :X."
+  [M P X]
+  (assert (contains? #{:X :R :Y} P) (str "P (" P ") is not one of :X :R :Y"))
+  (-> M
+      (desactivate-nodes)
+      (activate-node P)
+      (activate-node X)
+      (propagate) ))
 
 ;; (activeA ?M X) ->
 ; {L1 {isa Link, atoms [L3 L4], active true}
--- test/mema/core_test.clj
+++ test/mema/core_test.clj
@@ -1,6 +1,7 @@
 (ns mema.core-test
   (:require [clojure.test :refer :all]
-            [mema.core :as m]))
+            [mema.core :as m])
+  (:import  [clojure.lang ExceptionInfo]) )
 
 
 (def M-test
@@ -9,13 +10,14 @@
    :L3 {:isa :Link, :atoms [:L1 :L2]}
    :N1  {:isa :Node}
    :N2  {:isa :Node}
-   :N3  {:isa :Node}
+   :N3  {:isa :Node, :lex "N3"}
+   :lex=>node {"N3" :N3}
    :active-nodes #{:N1}
    :active-links #{:L1} })
 
 
 ; (pp-obj-dispatcher-test)
-(deftest pp-obj-dispatcher-test
+(deftest pp-obj-dispatcher-tests
   (testing "PP = :isa"
     (is (= [:isa :Link]
            (m/pp-obj-dispatcher M-test :isa :L1) ))
@@ -30,7 +32,7 @@
     ))
 
 ; (p-type-dispatcher-test)
-(deftest pp-type-dispatcher-test
+(deftest pp-type-dispatcher-tests
   (testing "PP = :is-active"
     (is (= [:is-active :Link]
            (m/pp-type-dispatcher M-test :is-active :Link) ))
@@ -39,7 +41,7 @@
     ))
 
 ; (get-test)
-(deftest get-test
+(deftest get-tests
   (testing "PP = :isa"
     (is (= :Link
            (m/get M-test :isa :L1) ))
@@ -58,7 +60,7 @@
     ))
 
 ; (get-all-test)
-(deftest get-all-test
+(deftest get-all-tests
   (testing "PP = :is-active"
     (is (= #{:N1}
            (m/get-all M-test :is-active :Node) ))
@@ -66,26 +68,73 @@
            (m/get-all M-test :is-active :Link) ))
     ))
 
+; (create-lex=>node-tests)
+(deftest create-lex=>node-tests
+  (testing "Nominal cases"
+    (let [M1 (m/create-lex=>node {} "3")
+          N  (get-in M1 [:lex=>node "3"]) ]
+      (is (some? N))
+      (is (= {:isa :Node :lex "3"}
+             (get M1 N) ))))
+  (testing "Checks : bad types for lex"
+    (is (thrown? AssertionError (m/create-lex=>node {} 3)))
+    (is (thrown? AssertionError (m/create-lex=>node {} :x)))
+    (is (thrown? AssertionError (m/create-lex=>node {} 'x))) ))
+
+; (activate-node-tests)
+(deftest activate-node-tests
+  (testing "Nominal cases"
+    (is (m/get (m/activate-node {:M M-test :N :N1})  :is-active :N1))
+    (is (m/get (m/activate-node {:M M-test :N :N2})  :is-active :N2))
+    (is (m/get (m/activate-node {:M M-test :N "N3"}) :is-active :N3)) )
+  (testing "Check bad cases"
+    (is (thrown? ExceptionInfo (m/activate-node {:M M-test :N :L1})) ))
+  )
 
 
 ;;; (equality-slides) cf doc/equality-states.pptx
 
 (deftest equality-slides
-  (let [M (atom {})]
+  (let [M0 {:L1 {:isa :Link, :atoms [:L3 :L4]}
+            :L3 {:isa :Link, :atoms [:R :N1]}
+            :L4 {:isa :Link, :atoms [:X :Y]}
+            :X  {:isa :Node}
+            :R  {:isa :Node}
+            :Y  {:isa :Node}
+            :N1 {:isa :Node, :io "="}
+            :io=>node {"=" :N1}
+            :active-nodes #{}
+            :active-links #{:L1} }
+        M  (atom nil)
+        N3 (atom nil)
+        L2 (atom nil) ]
     (testing "SLIDE 1 : init"
-      (reset! M  {:L1 {:isa :Link, :atoms [:L3 :L4]}
-                  :L3 {:isa :Link, :atoms [:R :N1]}
-                  :L4 {:isa :Link, :atoms [:X :Y]}
-                  :X  {:isa :Node}
-                  :R  {:isa :Node}
-                  :Y  {:isa :Node}
-                  :N1 {:isa :Node, :io "="}
-                  :io=>node {"=" :N1}
-                  :active-nodes #{}
-                  :active-links #{:L1} })
+      (reset! M M0)
       (is (= #{}
              (m/get-all @M :is-active :Node) ))
       (is (= #{:L1}
+             (m/get-all @M :is-active :Link) ))
+      )
+    (testing "SLIDE 2 : '*3* = ?'"
+      (swap! M m/desactivate-nodes)
+      (swap! M m/activate-node :X)
+      (is (m/get @M :is-active :X))
+      (swap! M m/activate-node "3")
+      (reset! N3 (m/get-lex=>node @M "3"))
+      (is (some? @N3))
+      (is (m/get @M :is-active @N3))
+      (is (= #{:X @N3}
+             (m/get-all @M :is-active :Node) ))
+      (is (= #{:L1}
+             (m/get-all @M :is-active :Link) )) )
+    (testing "SLIDE 3 : '*3* = ?'"
+      (swap! M m/propagate)
+      (reset! L2 (m/get-link @M :X @N3))
+      (is (some? @L2))
+      (is (m/get @M :is-active @L2))
+      (is (= #{:X @N3}
+             (m/get-all @M :is-active :Node) ))
+      (is (= #{:L1 @L2}
              (m/get-all @M :is-active :Link) ))
       )))
 
